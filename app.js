9f21dc952e46512e952ca2b69df307765068be6b
pascalhelbig <pascalhelbig@gmail.com>
2016-02-02 10:14:05 +0100

    load images from imgur

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var dotenv = require('dotenv');
+var request = require('request');
 
 var app = express();
 dotenv.load();
@@ -9,7 +10,27 @@ app.get('/', function (req, res) {
 });
 
 app.get('/imagesearch/:search', function (req, res) {
-  res.send(req.params.search);
+  var url = "https://api.imgur.com/3/gallery/search/top?q=" + req.params.search;
+  request({
+    url: url,
+    json: true,
+    headers: {
+      Authorization: 'Client-ID ' + process.env.IMGUR_CLIENT_ID
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var result = body.data.map(function(image) {
+        return {
+          url: image.link,
+          type: image.type,
+          title: image.title
+        };
+      });
+      res.json(result);
+    } else {
+      res.json({error: error, status: response.statusCode});
+    }
+  });
 });
 
 app.listen(8080, function () {
