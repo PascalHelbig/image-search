fc14780c864bcfefa622090585121b9473a6681b
pascalhelbig <pascalhelbig@gmail.com>
2016-02-02 09:47:13 +0100

    add imagesearch route without logic

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+app.get('/imagesearch/:search', function (req, res) {
+  res.send(req.params.search);
+});
+
 app.listen(8080, function () {
   console.log('App listening on port 8080');
 });
